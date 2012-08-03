b2474d1aa008f36d45ab6cdb1fba1c149d5f81c9
Kwan Hong Lee <kwan@redstar.com>
2012-08-03 17:36:18 -0400

    added list of addons

--- utils/addons.sh
+++ utils/addons.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-heroku addons:add redistogo:nano --app redfly-test
-heroku addons:add zerigo_dns:basic --app redfly-test
-heroku addons:add sendgrid:starter --app redfly-test
-heroku addons:add bonsai:test --app redfly-test
 heroku addons:add redistogo:nano --app redfly-dev
 heroku addons:add zerigo_dns:basic --app redfly-dev
 heroku addons:add sendgrid:starter --app redfly-dev
 heroku addons:add bonsai:test --app redfly-dev
+heroku addons:add mongolab:starter --app redfly-dev
+heroku addons:add memcachier:dev --app redfly-dev
+heroku addons:add redistogo:nano --app redfly-test
+heroku addons:add zerigo_dns:basic --app redfly-test
+heroku addons:add sendgrid:starter --app redfly-test
+heroku addons:add bonsai:test --app redfly-test
+heroku addons:add neo4j:test --app redfly-test
+heroku addons:add mongolab:starter --app redfly-test
+heroku addons:add memcachier:dev --app redfly-test
 heroku addons:add redistogo:nano --app redfly-staging
 heroku addons:add zerigo_dns:basic --app redfly-staging
 heroku addons:add sendgrid:starter --app redfly-staging
 heroku addons:add bonsai:test --app redfly-staging
+heroku addons:add mongolab:starter --app redfly-staging
+heroku addons:add memcachier:dev --app redfly-staging
 heroku addons:add redistogo:nano --app redfly-prod
 heroku addons:add zerigo_dns:basic --app redfly-prod
 heroku addons:add sendgrid:starter --app redfly-prod
 heroku addons:add bonsai:test --app redfly-prod
+heroku addons:add mongolab:starter --app redfly-prod
+heroku addons:add memcachier:dev --app redfly-prod
+
