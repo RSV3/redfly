9b38b31adb0d91da89e756485702b74efb9ec6e7
Krzysztof Baranowski <pharcosyle@gmail.com>
2012-08-08 18:06:00 -0400

    don't use Up it just makes a mess of things.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 require('coffee-script');
 
-require('derby').run(__dirname + '/lib/server');
+// require('derby').run(__dirname + '/lib/server', 3000, {numWorkers: 1, requires: ['coffee-script']});
+require('./lib/server').listen(3000);
