5f9f376755be2ac6528fa4240d3cc612d6e96910
Kwan Hong Lee <kwan@redstar.com>
2013-05-13 18:00:36 -0400

    script to deploy to staging after committing on master

---
+++ deploy_staging.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# run from the master branch after commit
+git push origin master
+git checkout staging
+git pull origin staging
+git merge master
+git push origin staging
+git push heroku-staging staging:master
