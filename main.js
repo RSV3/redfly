3e166b40ecfd632481eeec69f319b2f4916b627b
justin TNT <just@justat.at>
2013-07-31 23:21:20 +0930

    try to get nodefly / strong opts going on staging

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-if (process.env.NODETIME_ACCOUNT_KEY)
-   	require('nodetime').profile({accountKey: process.env.NODETIME_ACCOUNT_KEY});
+// profiler plugins:
+// should not have both of these at once
+var profile_config = {
+//	NODETIME_ACCOUNTKEY: '9ed3b83d70427d402822795e80426b7ec5343e2f'	
+	STRONG_AGENT_ID: '7f9fd8ac-056b-467b-94a1-12e2babdead9'
+};
 
-if (process.env.STRONG_AGENT_ID)
+if (profile_config.NODETIME_ACCOUNT_KEY) {
+	console.log('profiling with nodetime ' + profile_config.NODETIME_ACCOUNT_KEY);
+   	require('nodetime').profile({accountKey: profile_config.NODETIME_ACCOUNT_KEY});
+} else if (profile_config.STRONG_AGENT_ID) {
+	console.log('profiling with strong-agent ' + profile_config.STRONG_AGENT_ID);
    	require('strong-agent').profile(
-		    process.env.STRONG_AGENT_ID,
-		    [APPLICATION_NAME,'Heroku'],
-			{} // optional options
+		    profile_config.STRONG_AGENT_ID,
+		    ['redfly-staging', 'Heroku'],
+			{ blockThreshold: 1234 } // optional options
 	);
+}
 
 require('phrenetic/main');
