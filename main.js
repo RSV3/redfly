771c5311349fd6605a00b8dfb56249bcfa720d7a
justin TNT <just@justat.at>
2013-07-31 22:45:55 +0930

    relax parse on login; add strong agent to config

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 if (process.env.NODETIME_ACCOUNT_KEY)
    	require('nodetime').profile({accountKey: process.env.NODETIME_ACCOUNT_KEY});
 
+if (process.env.STRONG_AGENT_ID)
+   	require('strong-agent').profile(
+		    process.env.STRONG_AGENT_ID,
+		    [APPLICATION_NAME,'Heroku'],
+			{} // optional options
+	);
+
 require('phrenetic/main');
