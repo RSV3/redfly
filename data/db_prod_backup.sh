655063d3793816f68b1479de2b79bab8180117c2
Kwan Hong Lee <kwan@redstar.com>
2013-10-29 12:17:31 -0400

    added remove user script to cleanup any user that is not supposed to have logged in

---
+++ data/db_prod_backup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mongodump --host ds039147.mongolab.com:39147 --username heroku_app8065862 --password 6cqi48lldblomdf4uebuhplblj --db heroku_app8065862 --out redfly_dump_backup
+
+# to restore to prod, you need to change credentials
+#mongorestore --host ds043837.mongolab.com:43837 --username heroku_app6379653 --password 2vos1gak0e63rjl5220mluubm6 --db heroku_app6379653 redfly_dump_backup/heroku_app8065862
+
